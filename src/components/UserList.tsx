e34843d90ad3999e029fbeafc71ed38695ad4bbf
S.Zelensky <zelenstein@gmail.com>
2023-11-10 00:46:24 +0300

    fetchTodos

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { useTypesSelector } from '../hooks/useTypesSelector';
+import React, { useEffect } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const UserList: React.FC = () => {
-  const { users, error, loading } = useTypesSelector((state) => state.user);
+  const { users, error, loading } = useTypedSelector((state) => state.user);
+  const { fetchUsers } = useActions();
 
-  return <div>UserList</div>;
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <h1>Идёт загрузка...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
